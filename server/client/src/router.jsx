37612773a269419f3a216bc93368463762d4cfed
AbHaY-PaWaR <abhaypawar479@gmail.com>
2026-02-14 01:07:36 +0530

    Fix Special page import casing

--- server/client/src/router.jsx
+++ server/client/src/router.jsx
@@ -8,7 +8,7 @@ import CreateValentine from "./pages/CreateValentine";
 import ValentineView from "./pages/ValentineView";
 import RoseDay from "./component/ui/RoseDay";
 import ValentineDays from "./pages/ValentineDays";
-import Special from "./pages/special";
+import Special from "./pages/Special.jsx";
 
 const router = createBrowserRouter([
   {
